9e067005787ebed049f1996f8c64a64c972bc69f
Esmahr <esmaharmanci40@gmail.com>
2024-02-20 17:59:04 +0300

    dashboard düzenlendi

--- src/app/blog-edit/blog-edit.component.ts
+++ src/app/blog-edit/blog-edit.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import EditorJS from '@editorjs/editorjs';
+import { DescriptionService } from '../services/description.service';
 
 @Component({
   selector: 'app-blog-edit',
   templateUrl: './blog-edit.component.html',
   styleUrls: ['./blog-edit.component.css']
 })
-export class BlogEditComponent {
+export class BlogEditComponent implements AfterViewInit {
+  @ViewChild('editor', { static: true }) editorElement!: ElementRef;
+
+  private editor: EditorJS | undefined;
+
+  constructor(private descriptionService: DescriptionService) { }
+
+  ngAfterViewInit(): void {
+    this.initializeEditor();
+  }
+  private initializeEditor(): void {
+    this.editor = new EditorJS({
+      minHeight: 200,
+      holder: this.editorElement.nativeElement,
+      tools: {
+      }
+    });
+
+  }
+
+  showEditorData(): void {
+    this.editor?.save().then(data => {
+      console.dir(data);
+      this.descriptionService.postData(data);
+    });
+  }
 
 }
